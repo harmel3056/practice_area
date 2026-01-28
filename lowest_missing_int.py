afa7f1ea97020ca78ba38b86efe8493444d2164a
harmel3056 <melh1988@hotmail.com>
2026-01-29 09:41:11 +1100

    Update notes

--- lowest_missing_int.py
+++ lowest_missing_int.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
 """
-Create a function which, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
-For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+Create a function which, given an array A of N integers, returns the smallest positive
+integer (greater than 0) that does not occur in A.
+For example, given A = [1, 3, 6, 4, 1, 2], the function 
+should return 5.
 Given A = [1, 2, 3], the function should return 4.
 Given A = [−1, −3], the function should return 1.
 Write an efficient algorithm for the following assumptions:
-N is an integer within the range [1..100,000]; each element of array A is an integer within the range [−1,000,000..1,000,000].
+N is an integer within the range [1..100,000]; each element of array A is an integer
+within the range [−1,000,000..1,000,000].
 """
 
 def solution(A):
     N = len(A) # total number of indices in array
-    present = [False] * (N + 1) # creates an array of 'False' boolean values to use as switches for every number, +1 accoms the 0 array item
+    present = [False] * (N + 1)
+    # creates an array of 'False' boolean values to use as switches for every number, +1 accoms the 0 array item
 
     # mark the presence of each valid positive integer
     for x in A:
